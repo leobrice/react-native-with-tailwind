b96fa7b9388a4a5453d7234bef21ba418dded359
leobrice <mushibrayson07@gmail.com>
2024-03-16 16:57:00 +0300

    React native and tailwind starting

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState,useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const app = ()=> {
+  const[count,setCount]=useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="bg-blue-200">
+      <SafeAreaView  className="bg-red-200">
+        <View>
+          <Text>You have pressed {count} times</Text>
+          <StatusBar />
+          <Button
+            title="Add +"
+            color="#841584"
+            onPress={() => {
+              setCount(count + 1);
+            }}
+          ></Button>
+          <Button
+            title="Clear"
+            onPress={() => {
+              setCount(0);
+            }}
+          ></Button>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default app;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [],
-   content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./App.{js,jsx,ts,tsx}", "./src/*/.{js,jsx,ts,tsx}", "./screens/*/.{js,jsx,ts,tsx}"],
+ theme: {
+   extend: {},
+ },
+ plugins: [],
+}
